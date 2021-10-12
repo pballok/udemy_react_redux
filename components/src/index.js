d2dbfc2995c0abf58d784a1cf5754dce42587eed
Peter Ballok <pballok@gmail.com>
2021-10-12 15:45:20 -0400

    wrap each Components in ApprovalCard

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Bob"
-        avatarSource={faker.image.avatar()}
-        timeAgo="Today at 4:45PM"
-        commentText="Hello world"
-      />
-      <CommentDetail
-        author="Chris"
-        avatarSource={faker.image.avatar()}
-        timeAgo="Today at 2:00AM"
-        commentText="Does this work?"
-      />
-      <CommentDetail
-        author="Joe"
-        avatarSource={faker.image.avatar()}
-        timeAgo="Yesterday at 5:00PM"
-        commentText="I think it does"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Bob"
+          avatarSource={faker.image.avatar()}
+          timeAgo="Today at 4:45PM"
+          commentText="Hello world"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Chris"
+          avatarSource={faker.image.avatar()}
+          timeAgo="Today at 2:00AM"
+          commentText="Does this work?"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Joe"
+          avatarSource={faker.image.avatar()}
+          timeAgo="Yesterday at 5:00PM"
+          commentText="I think it does"
+        />
+      </ApprovalCard>
     </div>
   );
 };
