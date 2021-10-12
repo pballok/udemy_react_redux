5d0003d74afa85cecc2e5c2b905db7789093c659
Peter Ballok <pballok@gmail.com>
2021-10-12 12:50:47 -0400

    use multilpe props for Component

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Bob" />
-      <CommentDetail author="Chris" />
-      <CommentDetail author="Joe" />
+      <CommentDetail
+        author="Bob"
+        avatarSource={faker.image.avatar()}
+        timeAgo="Today at 4:45PM"
+        commentText="Hello world"
+      />
+      <CommentDetail
+        author="Chris"
+        avatarSource={faker.image.avatar()}
+        timeAgo="Today at 2:00AM"
+        commentText="Does this work?"
+      />
+      <CommentDetail
+        author="Joe"
+        avatarSource={faker.image.avatar()}
+        timeAgo="Yesterday at 5:00PM"
+        commentText="I think it does"
+      />
     </div>
   );
 };
