f37e0293b6fd4507a7d5dd2ef5d1291751fa1caa
Peter Ballok <pballok@gmail.com>
2021-10-14 16:09:18 -0400

    use a Spinner loader

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
 
@@ -16,7 +17,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
@@ -25,7 +26,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading...</div>
+    return <Spinner message="Please accept location request" />
+  }
+
+  render() {
+    return this.renderContent();
   }
 }
 
