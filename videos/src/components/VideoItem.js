4305e65f7ed80db9b10c23f8bdd7de880ff6a93a
Peter Ballok <pballok@gmail.com>
2021-10-19 20:24:32 -0400

    select video when clicked

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -2,13 +2,13 @@ import './VideoItem.css';
 
 import React from "react";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
+    <div className="video-item item" onClick={() => onVideoSelect(video) }>
       <img
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
-        alt={video.snippet.thumbnails.title}
+        alt={video.snippet.title}
       />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -2,9 +2,15 @@ import React from "react";
 
 import VideoItem from "./VideoItem";
 
-const VideoList = ({videos}) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedVideos = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        video={video}
+        onVideoSelect={onVideoSelect}
+      />
+    );
   });
 
   return (
