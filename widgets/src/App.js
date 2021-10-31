ca4f5ab7c9a76f6b5e621777078ea44d5390f5ec
Peter Ballok <pballok@gmail.com>
2021-10-31 17:01:23 -0400

    add widget Dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -18,10 +19,25 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+];
+
 const App = () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options}/>
     </div>
   );
 };
