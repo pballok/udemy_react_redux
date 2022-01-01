9f82a0eea3935f81bb348e946dbb8ec6a201f920
Peter Ballok <pballok@gmail.com>
2022-01-01 17:24:37 -0500

    scaffold Translate widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -35,15 +36,9 @@ const options = [
 ];
 
 const App = () => {
-const [selected, setSelected] = useState(options[0]);
-
   return (
     <div>
-      <Dropdown
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-      />
+      <Translate />
     </div>
   );
 };
