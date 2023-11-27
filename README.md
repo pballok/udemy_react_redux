6c2cb5def5b064eb4320b4b9944bd4df680e8874
pballok <pballok@gmail.com>
2023-11-26 22:33:12 -0500

    fix: update jsx project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-My code as I follow along the `Modern React with Redux [2020 update]` course on Udemy: https://www.udemy.com/course/react-redux/
+My code as I follow along the `Modern React with Redux [2023 update]` course on Udemy: https://www.udemy.com/course/react-redux/
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,24 +1,13 @@
+// 1) Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const App = () => {
-  const buttonText = { text: 'Click me' };
-  const labelText = 'Enter name:';
+// 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
 
-  return (
-    <div>
-      <label className="label" htmlFor="name">
-        {labelText}
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonText.text}
-      </button>
-    </div>
-  );
-};
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+// 4) Show the component on the screen
+root.render(<App />);
