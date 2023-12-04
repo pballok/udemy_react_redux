0cbc52a645dd084ed16cbe2dc3b8f9a64adcf2a9
pballok <pballok@gmail.com>
2023-12-04 09:54:06 -0500

    feat: add new project animals

---
+++ animals/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const el = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(el);
+
+root.render(<App />);
