1544eef22f85ad1d938b1a9bda15809945bb24bb
pballok <pballok@gmail.com>
2024-02-16 09:18:43 -0500

    feat: implement animals

--- animals/src/App.tsx
+++ animals/src/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+
+import './App.css';
+
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-    return <div>Show animal list here!</div>;
+    const [animals, setAnimals] = useState<string[]>([]);
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    }
+
+    const renderedAnimals = animals.map((animal, index) =>
+        <AnimalShow type={animal} key={index} />
+    );
+
+    return (
+        <div className="app">
+            <button onClick={handleClick}>Add Animal</button>
+            <div className="animal-list">{renderedAnimals}</div>
+        </div>
+    );
 }
 
 export default App;
